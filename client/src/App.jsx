baf7f1ceee0d297582447325a2ec26d5ed165ae7
mfadhili <mfadhili@github.com>
2024-10-10 14:53:40 +0300

    finished plad api tutorial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,26 +2,85 @@ import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from "axios";
+import { usePlaidLink } from 'react-plaid-link';
 // import './App.css'
 
 axios.defaults.baseURL="http://localhost:8000"
-function App() {
 
-  useEffect(
+
+function PlaidAuth({publicToken}) {
+    const [account, setAccount] = useState()
+
+    useEffect(() => {
+        async function fetch() {
+            let access_token = await axios.post("/exchange_public_token",{
+                public_token: publicToken,
+            });
+            console.log("access Token: ",access_token.data)
+
+            const auth = await axios.post("/authenticate", {access_token: access_token.data.accessToken});
+            console.log("auth data ", auth.data)
+            setAccount(auth.data.numbers.ach[0])
+        }
+
+        fetch();
+    }, []);
+
+    return account && (
+        <div>
+                <p>
+                    Account Number: {account.account}
+                </p>
+                <p>
+                    Account routing number: {account.routing}
+                </p>
+        </div>
+    )
+
+    ;
+}
+
+function App() {
+    const [linkToken, setLinkToken] = useState('')
+    const [publicToken, setPublicToken] = useState()
+
+    useEffect(
       () => {
         async function fetch() {
-          const resp = await axios.post("/echo", {name: "mfadhili"});
+          const resp = await axios.post("/create_link_token");
+          setLinkToken(resp.data.link_token)
             console.log("response: ", resp.data)
         }
 
         fetch()
       }, []
-  );
+    );
 
 
-  return (
-    <div> Client Page</div>
-  )
+
+    const { open, ready } = usePlaidLink({
+        token: linkToken,
+        onSuccess: (public_token, metadata) => {
+            // send public_token to server
+            setPublicToken(public_token)
+            console.log("success : ", public_token, metadata)
+        },
+    });
+
+
+  return publicToken ?
+      (
+          <PlaidAuth publicToken={publicToken}></PlaidAuth>
+      ):
+      (
+          <div>
+              Client Page
+              <button onClick={() => open()} disabled={!ready}>
+                  Connect a bank account
+              </button>
+          </div>
+      )
+
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express= require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const keys = require("keys")
+// const keys = require("keys")
 
 const  { Configuration, PlaidApi, PlaidEnvironments } =  require('plaid');
 
@@ -9,11 +9,15 @@ const configuration = new Configuration({
     basePath: PlaidEnvironments.sandbox,
     baseOptions: {
         headers: {
-            'PLAID-CLIENT-ID': keys.PLAID_CLIENT_ID,
-            'PLAID-SECRET': keys.PLAID_SECRET,
+            'PLAID-CLIENT-ID': '6707aa4eecfe30001a6edc5d',
+            'PLAID-SECRET': 'af624199c70ae5fd9dd1fc033e4ba5'
         },
     },
 });
+
+const plaidClient = new PlaidApi(configuration);
+
+
 
 const app = express()
 app.use(cors())
@@ -26,6 +30,71 @@ app.get("/hello", (req, res) => {
 app.post("/echo", (req, res) => {
     const name = req.body.name
     res.json({name: name})
+})
+
+
+app.post('/create_link_token', async function (request, response) {
+    // Get the client_user_id by searching for the current user
+    // const user = await User.find(...);
+    // const clientUserId = user.id;
+    const plaidRequest = {
+        user: {
+            // This should correspond to a unique id for the current user.
+            client_user_id: "user",
+        },
+        client_name: 'Plaid Test App',
+        products: ['auth'],
+        language: 'en',
+        redirect_uri: 'http://localhost:3000/',
+        country_codes: ['US'],
+    };
+    try {
+        const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+        response.json(createTokenResponse.data);
+    } catch (error) {
+        // handle error
+        console.error(error)
+        response.status(500).json({})
+    }
+});
+
+app.post('/exchange_public_token', async function (
+    request,
+    response,
+    next,
+) {
+    const publicToken = request.body.public_token;
+    console.log("publicToken: ",publicToken)
+    try {
+        const plaidResponse = await plaidClient.itemPublicTokenExchange({
+            public_token: publicToken,
+        });
+        // These values should be saved to a persistent database and
+        // associated with the currently signed-in user
+        const accessToken = plaidResponse.data.access_token;
+        const itemID = plaidResponse.data.item_id;
+
+        console.log("response: ", response )
+        response.json({ accessToken: accessToken });
+    } catch (error) {
+        // handle error
+        response.status(500).json("failed")
+    }
+});
+
+app.post('/authenticate', async function (request, response) {
+    try {
+        const accessToken = request.body.access_token;
+        console.log("access token: " ,accessToken)
+        const plaidRequestBody = {
+            access_token: accessToken,
+        }
+        const plaidResponse = await plaidClient.authGet(plaidRequestBody)
+        response.json(plaidResponse.data);
+    } catch (error) {
+        // console.error(error)
+        response.status(500).json("failed")
+    }
 })
 
 app.listen(8000, () => {
